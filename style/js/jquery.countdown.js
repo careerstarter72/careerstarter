20bb3896aafa521cf0c898efcec3b9a369f1ac28
insideseed <jayarammasetti@gmail.com>
2016-04-12 18:05:49 +0530

    updated 6:00pm.

--- style/js/jquery.countdown.js
+++ style/js/jquery.countdown.js
@@ -1,200 +1,112 @@
-/*!
- * The Final Countdown for jQuery v2.0.2 (http://hilios.github.io/jQuery.countdown/)
- * Copyright (c) 2013 Edson Hilios
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-(function(factory) {
-    if (typeof define === "function" && define.amd) {
-        define([ "jquery" ], factory);
-    } else {
-        factory(jQuery);
-    }
-})(function($) {
-    "use strict";
-    var PRECISION = 100;
-    var instances = [], matchers = [];
-    matchers.push(/^[0-9]*$/.source);
-    matchers.push(/([0-9]{1,2}\/){2}[0-9]{4}( [0-9]{1,2}(:[0-9]{2}){2})?/.source);
-    matchers.push(/[0-9]{4}(\/[0-9]{1,2}){2}( [0-9]{1,2}(:[0-9]{2}){2})?/.source);
-    matchers = new RegExp(matchers.join("|"));
-    function parseDateString(dateString) {
-        if (dateString instanceof Date) {
-            return dateString;
-        }
-        if (String(dateString).match(matchers)) {
-            if (String(dateString).match(/^[0-9]*$/)) {
-                dateString = Number(dateString);
-            }
-            return new Date(dateString);
-        } else {
-            throw new Error("Couldn't cast `" + dateString + "` to a date object.");
-        }
-    }
-    var DIRECTIVE_KEY_MAP = {
-        Y: "years",
-        m: "months",
-        w: "weeks",
-        d: "days",
-        D: "totalDays",
-        H: "hours",
-        M: "minutes",
-        S: "seconds"
-    };
-    function strftime(offsetObject) {
-        return function(format) {
-            var directives = format.match(/%(-|!)?[A-Z]{1}(:[^;]+;)?/gi);
-            if (directives) {
-                for (var i = 0, len = directives.length; i < len; ++i) {
-                    var directive = directives[i].match(/%(-|!)?([a-zA-Z]{1})(:[^;]+;)?/), regexp = new RegExp(directive[0]), modifier = directive[1] || "", plural = directive[3] || "", value = null;
-                    directive = directive[2];
-                    if (DIRECTIVE_KEY_MAP.hasOwnProperty(directive)) {
-                        value = DIRECTIVE_KEY_MAP[directive];
-                        value = Number(offsetObject[value]);
-                    }
-                    if (value !== null) {
-                        if (modifier === "!") {
-                            value = pluralize(plural, value);
-                        }
-                        if (modifier === "") {
-                            if (value < 10) {
-                                value = "0" + value.toString();
-                            }
-                        }
-                        format = format.replace(regexp, value.toString());
-                    }
-                }
-            }
-            format = format.replace(/%%/, "%");
-            return format;
-        };
-    }
-    function pluralize(format, count) {
-        var plural = "s", singular = "";
-        if (format) {
-            format = format.replace(/(:|;|\s)/gi, "").split(/\,/);
-            if (format.length === 1) {
-                plural = format[0];
-            } else {
-                singular = format[0];
-                plural = format[1];
-            }
-        }
-        if (Math.abs(count) === 1) {
-            return singular;
-        } else {
-            return plural;
-        }
-    }
-    var Countdown = function(el, finalDate, callback) {
-        this.el = el;
-        this.$el = $(el);
-        this.interval = null;
-        this.offset = {};
-        this.setFinalDate(finalDate);
-        this.instanceNumber = instances.length;
-        instances.push(this);
-        this.$el.data("countdown-instance", this.instanceNumber);
-        if (callback) {
-            this.$el.on("update.countdown", callback);
-            this.$el.on("stoped.countdown", callback);
-            this.$el.on("finish.countdown", callback);
-        }
-        this.start();
-    };
-    $.extend(Countdown.prototype, {
-        start: function() {
-            if (this.interval !== null) {
-                throw new Error("Countdown is already running!");
-            }
-            var self = this;
-            this.update();
-            this.interval = setInterval(function() {
-                self.update.call(self);
-            }, PRECISION);
-        },
-        stop: function() {
-            clearInterval(this.interval);
-            this.interval = null;
-            this.dispatchEvent("stoped");
-        },
-        pause: function() {
-            this.stop.call(this);
-        },
-        resume: function() {
-            this.start.call(this);
-        },
-        remove: function() {
-            this.stop();
-            delete instances[this.instanceNumber];
-        },
-        setFinalDate: function(value) {
-            this.finalDate = parseDateString(value);
-        },
-        update: function() {
-            if (this.$el.closest("html").length === 0) {
-                this.remove();
-                return;
-            }
-            this.totalSecsLeft = this.finalDate.valueOf() - new Date().valueOf();
-            this.totalSecsLeft = Math.ceil(this.totalSecsLeft / 1e3);
-            this.totalSecsLeft = this.totalSecsLeft < 0 ? 0 : this.totalSecsLeft;
-            this.offset = {
-                seconds: this.totalSecsLeft % 60,
-                minutes: Math.floor(this.totalSecsLeft / 60) % 60,
-                hours: Math.floor(this.totalSecsLeft / 60 / 60) % 24,
-                days: Math.floor(this.totalSecsLeft / 60 / 60 / 24) % 7,
-                totalDays: Math.floor(this.totalSecsLeft / 60 / 60 / 24),
-                weeks: Math.floor(this.totalSecsLeft / 60 / 60 / 24 / 7),
-                months: Math.floor(this.totalSecsLeft / 60 / 60 / 24 / 30),
-                years: Math.floor(this.totalSecsLeft / 60 / 60 / 24 / 365)
-            };
-            if (this.totalSecsLeft === 0) {
-                this.stop();
-                this.dispatchEvent("finish");
-            } else {
-                this.dispatchEvent("update");
-            }
-        },
-        dispatchEvent: function(eventName) {
-            var event = $.Event(eventName + ".countdown");
-            event.finalDate = this.finalDate;
-            event.offset = $.extend({}, this.offset);
-            event.strftime = strftime(this.offset);
-            this.$el.trigger(event);
-        }
-    });
-    $.fn.countdown = function() {
-        var argumentsArray = Array.prototype.slice.call(arguments, 0);
-        return this.each(function() {
-            var instanceNumber = $(this).data("countdown-instance");
-            if (instanceNumber !== undefined) {
-                var instance = instances[instanceNumber], method = argumentsArray[0];
-                if (Countdown.prototype.hasOwnProperty(method)) {
-                    instance[method].apply(instance, argumentsArray.slice(1));
-                } else if (String(method).match(/^[$A-Z_][0-9A-Z_$]*$/i) === null) {
-                    instance.setFinalDate.call(instance, method);
-                } else {
-                    $.error("Method %s does not exist on jQuery.countdown".replace(/\%s/gi, method));
-                }
-            } else {
-                new Countdown(this, argumentsArray[0], argumentsArray[1]);
-            }
-        });
-    };
-});
+// CountDown Clock
+// Version   : 1.0.1
+// Developer : Ekrem KAYA
+// Website   : http://e-piksel.com
+// GitHub    : https://github.com/epiksel/countdown
+
+(function ($) {
+	$.fn.countdown = function (options, callback) {
+		var settings = $.extend({
+			date: null,
+			offset: null,
+			day: 'Day',
+			days: 'Days',
+			hour: 'Hour',
+			hours: 'Hours',
+			minute: 'Minute',
+			minutes: 'Minutes',
+			second: 'Second',
+			seconds: 'Seconds'
+		}, options);
+
+		// Throw error if date is not set
+		if (!settings.date) {
+			$.error('Date is not defined.');
+		}
+
+		// Throw error if date is set incorectly
+		if (!Date.parse(settings.date)) {
+			$.error('Incorrect date format, it should look like this, 12/24/2012 12:00:00.');
+		}
+
+		// Save container
+		var container = this;
+
+		/**
+		 * Change client's local date to match offset timezone
+		 * @return {Object} Fixed Date object.
+		 */
+		var currentDate = function () {
+			// get client's current date
+			var date = new Date();
+
+			// turn date to utc
+			var utc = date.getTime() + (date.getTimezoneOffset() * 60000);
+
+			// set new Date object
+			var new_date = new Date(utc + (3600000*settings.offset));
+
+			return new_date;
+		};
+
+		/**
+		 * Main countdown function that calculates everything
+		 */
+		function countdown () {
+			var target_date = new Date(settings.date), // set target date
+				current_date = currentDate(); // get fixed current date
+
+			// difference of dates
+			var difference = target_date - current_date;
+
+			// if difference is negative than it's pass the target date
+			if (difference < 0) {
+				// stop timer
+				clearInterval(interval);
+
+				if (callback && typeof callback === 'function') callback();
+
+				return;
+			}
+
+			// basic math variables
+			var _second = 1000,
+				_minute = _second * 60,
+				_hour = _minute * 60,
+				_day = _hour * 24;
+
+			// calculate dates
+			var days = Math.floor(difference / _day),
+				hours = Math.floor((difference % _day) / _hour),
+				minutes = Math.floor((difference % _hour) / _minute),
+				seconds = Math.floor((difference % _minute) / _second);
+		
+			// based on the date change the refrence wording
+			var text_days = (days === 1) ? settings.day : settings.days,
+				text_hours = (hours === 1) ? settings.hour : settings.hours,
+				text_minutes = (minutes === 1) ? settings.minute : settings.minutes,
+				text_seconds = (seconds === 1) ? settings.second : settings.seconds;
+
+				// fix dates so that it will show two digets
+				days = (String(days).length >= 2) ? days : '0' + days;
+				hours = (String(hours).length >= 2) ? hours : '0' + hours;
+				minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
+				seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
+
+			// set to DOM
+			container.find('.days').text(days);
+			container.find('.hours').text(hours);
+			container.find('.minutes').text(minutes);
+			container.find('.seconds').text(seconds);
+
+			container.find('.days_text').text(text_days);
+			container.find('.hours_text').text(text_hours);
+			container.find('.minutes_text').text(text_minutes);
+			container.find('.seconds_text').text(text_seconds);
+		}
+		
+		// start
+		var interval = setInterval(countdown, 1000);
+	};
+
+})(jQuery);
